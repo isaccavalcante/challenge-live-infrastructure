c06be9cffd0e30244af3e4eb430f1c721e3f7014
Isac Cavalcante <isac.cavalcante@hp.com>
2020-11-28 22:58:41 -0300

    Add packer file and provisioner scripts

--- packer/logstash.sh
+++ packer/logstash.sh
@@ -1,9 +1,8 @@
-
-
+#!/bin/bash
 
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-'''
+sudo bash -c 'cat << EOF > /etc/yum.repos.d/logstash.repo
 [logstash-7.x]
 name=Elastic repository for 7.x packages
 baseurl=https://artifacts.elastic.co/packages/7.x/yum
@@ -12,12 +11,13 @@ gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
 type=rpm-md
-'''
+EOF'
 
 sudo yum install -y logstash
-sudo yum install -y httpd
+
+sudo /usr/share/logstash/bin/logstash-plugin install logstash-output-amazon_es
 
 
-/usr/share/logstash/bin/logstash-plugin install logstash-output-amazon_es
-
-
+sudo cp httpd-pipeline.conf /etc/logstash/conf.d/
+sudo systemctl enable logstash
+sudo systemctl start logstash
