9f29df94f78f4ade93de82cdb8c03cccbf23b78a
Isac Cavalcante <isac.cavalcante@hp.com>
2020-11-29 12:47:14 -0300

    Update packer scripts

--- packer/scripts/apache.sh
+++ packer/scripts/apache.sh
@@ -5,9 +5,9 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd.service
 
-sudo chmod 755 /var/log/httpd/
+sudo chmod -R 755 /var/log/httpd/
 
 sudo touch /var/www/html/index.html
-sudo chmod 755 /var/www/html/index.html
+sudo chmod 777 /var/www/html/index.html
 
 echo "<h1>Pagar.me Challenge</h1>" > /var/www/html/index.html
